714b207a36e9da821338e0ebc3e3fbb8c10c03aa
xiaoyihao001018 <xyh15814450869@outlook.com>
2024-07-31 11:42:02 +0800

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { invoke } from "@tauri-apps/api/core";
+import { check } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  greetingMessage = "";
+
+  async ngOnInit(): Promise<void> {
+    const update = await check();
+    if (update?.available) {
+      await update.downloadAndInstall();
+      await relaunch();
+    }
+  }
+  greet(event: SubmitEvent, name: string): void {
+    event.preventDefault();
+
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    invoke<string>("greet", { name }).then((text) => {
+      this.greetingMessage = text;
+    });
+  }
+}
